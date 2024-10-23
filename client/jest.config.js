1e03a9e519ffee31e092db342dd7c00ed10387a7
Ishan Gohil <ishan10.gohil@gmail.com>
2024-10-23 15:14:15 -0600

    integrated tests for ensuring test setup and server connection on frontEnd

---
+++ client/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    transform: {
+      '^.+\\.(ts|tsx)$': 'ts-jest',  // Transform TypeScript files with ts-jest
+      '^.+\\.(js|jsx|mjs)$': 'babel-jest', // Use babel-jest for JavaScript files, including ES modules
+    },
+    transformIgnorePatterns: [
+      '/node_modules/(?!axios)/',   // Don't ignore transforming axios or other ES modules
+    ],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  };
+  
