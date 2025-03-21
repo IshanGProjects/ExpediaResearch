2b1ea82a4661c046df8272c140255bcc3b64c7b3
James Vu <jamesvu@Jamess-Laptop.local>
2025-03-20 19:00:45 -0600

    Fixed skeleton loader.

--- client/src/components/CardSkeleton.tsx
+++ client/src/components/CardSkeleton.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Skeleton, Stack } from "@mui/material";
 
 const CardSkeleton = () => {
-  return ( 
-    <Stack spacing={1} sx={{ width: "100%", maxWidth: 450 }}>
-        <Skeleton variant="rectangular" height={170} sx={{ width: "100%" }} />
-        <Skeleton variant="text" />
-        <Skeleton variant="text" />
+  return (
+    <Stack spacing={1} sx={{ width: "100%", minHeight: 250, maxWidth: 450 }}>
+      <Skeleton variant="rectangular" height={170} sx={{ width: "100%" }} />
+      <Skeleton variant="text" />
+      <Skeleton variant="text" />
     </Stack>
   );
 };
